c650e2481ad6c6329eb79581a3e4ef8b56fd1fc3
ramon <jose.ramon.silva@ccc.ufcg.edu.br>
2020-08-20 03:24:13 -0300

    Checking out

---
+++ precache-manifest.26fa71d87cd0b58cf2b4849623d41382.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da80906d10c139c2d2ee10af90d8756c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6fdce0202b6b4509cd51",
+    "url": "/static/js/2.ec927785.chunk.js"
+  },
+  {
+    "revision": "bc47917250593033d10ca941512c5bc1",
+    "url": "/static/js/2.ec927785.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e9b5774890ed260975c",
+    "url": "/static/js/main.80376bc6.chunk.js"
+  },
+  {
+    "revision": "f300622784f73efdaed6",
+    "url": "/static/js/runtime-main.6b8df914.js"
+  },
+  {
+    "revision": "ef61e4666dbe4385f71b0dc97d863c17",
+    "url": "/static/media/pg.ef61e466.png"
+  },
+  {
+    "revision": "7760c2b78fe2cea9e2642b804d5a8180",
+    "url": "/static/media/pre.7760c2b7.png"
+  },
+  {
+    "revision": "27d1019a4da07b6305cb6006170d1ec2",
+    "url": "/static/media/prof_pic.27d1019a.jpg"
+  },
+  {
+    "revision": "9f87ff0787cd5ed0ac1db617bec937a0",
+    "url": "/static/media/zanky.9f87ff07.png"
+  }
+]);
